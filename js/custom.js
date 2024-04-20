a31706d23d2fce41af3b111ce220198e628987cc
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2024-04-20 18:43:01 +0500

    ww

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,52 @@
 
-  (function ($) {
-  
+(function ($) {
+
   "use strict";
 
-    // AOS ANIMATIONS
-    AOS.init();
+  // AOS ANIMATIONS
+  AOS.init();
 
-    // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  let delay = 1000
+  // NAVBAR
+  $('.navbar-nav .nav-link').click(function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
-    // NEWS IMAGE RESIZE
-    function NewsImageResize(){
-      $(".navbar").scrollspy({ offset: -76 });
-      
-      var LargeImage = $('.large-news-image').height();
+  // NEWS IMAGE RESIZE
+  function NewsImageResize() {
+    $(".navbar").scrollspy({ offset: -76 });
 
-      var MinusHeight = LargeImage - 6;
+    var LargeImage = $('.large-news-image').height();
 
-      $('.news-two-column').css({'height' : (MinusHeight - LargeImage / 2) + 'px'});
+    var MinusHeight = LargeImage - 6;
+
+    $('.news-two-column').css({ 'height': (MinusHeight - LargeImage / 2) + 'px' });
+
+    if (window.innerWidth <= 766) delay = 0
+
+    if (window.innerWidth <= 992) {
+      document.querySelectorAll('[data-aos="fade-up"]').forEach(el => el.dataset.aosDelay = 100)
     }
+  }
 
-    $(window).on("resize", NewsImageResize);
-    $(document).on("ready", NewsImageResize);
+  $(window).on("resize", NewsImageResize);
+  $(document).on("ready", NewsImageResize);
 
-    $('a[href*="#"]').click(function (event) {
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top - 66
-          }, 1000);
-        }
+  $('a[href*="#"]').click(function (event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 66
+        }, delay);
       }
-    });
-    
-  })(window.jQuery);
+    }
+  });
 
-  
+})(window.jQuery);
+
+
+
